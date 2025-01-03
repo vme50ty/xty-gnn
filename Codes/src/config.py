52581dd207cf7b8aa304ebb572b3c17e1165ab72
vme50ty <15116908166@163.com>
2025-01-03 09:29:44 +0800

    增加了requirements.txt

--- Codes/src/config.py
+++ Codes/src/config.py
@@ -2,7 +2,7 @@
 Author: lee12345 15116908166@163.com
 Date: 2024-10-28 10:22:17
 LastEditors: lee12345 15116908166@163.com
-LastEditTime: 2025-01-02 14:58:16
+LastEditTime: 2025-01-02 15:33:52
 FilePath: /Gnn/DHGNN-LSTM/Codes/src/config.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -10,7 +10,7 @@ import torch
 
 class Config():
     def __init__(self):
-        self.dataPath="../datas_Slow"
+        self.dataPath="../datas_Steal"
         self.device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.class_weights=[1.0, 1.0, 1.0]
         self.graph_nums=10
@@ -18,7 +18,7 @@ class Config():
         self.hidden_dim = 256
         self.learning_rate = 0.001
         self.epochs=50
-        self.test_ratio=0.8
+        self.test_ratio=0.2
         self.batch_size=1
         
         self.redis_host="10.29.202.222"
